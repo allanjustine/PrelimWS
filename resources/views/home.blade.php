207b4c668a869081af2cd59d342e2121c94d899a
allanjustine <labya200@gmail.com>
2021-10-05 22:15:02 +0800

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    img {
+        width: 1500px;
+    }
+    </style>
 <div class="container">
     <div class="row">
-
+        <img src="/picture/car.jpg" alt="">
 
     </div>
 </div>
